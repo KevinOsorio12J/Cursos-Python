068749e43a80d5093e80e51626a4c20be5eac083
Edlin25 <57233832+Edlin25@users.noreply.github.com>
2021-06-07 15:46:56 -0500

    Subir nuevos cambios

---
+++ Hola_Mundo.py
@@ -0,0 +1,32 @@
+#Python es un lenguaje de tipado dinámico
+
+'''
+El tiupado dinámico es que una misma variable puede contener 
+diferentes tipos de datos a lo largo de un programa.
+'''
+
+print("Hola Python again, \nMi nombre es Edlin Salas")
+
+valor= True
+nombre= 'Paola'
+
+numero= 105
+numero2= 10
+numero= 99
+numero2= 80
+print(type(numero))
+print(type(nombre))
+print(type(numero2))
+print(type(valor))
+print("El resultado es:",numero)
+
+#Operadores Aritméticos
+
+num = 10
+num2= 30
+
+resultado = num + num2
+
+print(resultado)
+
+#Python permite el tipado dinámico
